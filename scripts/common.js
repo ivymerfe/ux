31f3732ac36abc94a1eedac702dff34685858346
ivymerfe <82306640+ivymerfe@users.noreply.github.com>
2026-04-13 22:45:51 +0700

    up

--- scripts/common.js
+++ scripts/common.js
@@ -6,85 +6,100 @@ let directionUp = 0;
 let directionDown = 0;
 
 function scrollUp() {
-    if (scrollElement) {
-        scrollElement.scrollBy(0, -SCROLL_SPEED);
-        scrollAnimationFrame = requestAnimationFrame(scrollUp);
-    }
+  if (scrollElement) {
+    scrollElement.scrollBy(0, -SCROLL_SPEED);
+    scrollAnimationFrame = requestAnimationFrame(scrollUp);
+  }
 }
 function scrollDown() {
-    if (scrollElement) {
-        scrollElement.scrollBy(0, SCROLL_SPEED);
-        scrollAnimationFrame = requestAnimationFrame(scrollDown);
-    }
+  if (scrollElement) {
+    scrollElement.scrollBy(0, SCROLL_SPEED);
+    scrollAnimationFrame = requestAnimationFrame(scrollDown);
+  }
 }
 function stopScrolling() {
-    if (scrollAnimationFrame) {
-        cancelAnimationFrame(scrollAnimationFrame);
-        scrollAnimationFrame = null;
-    }
+  if (scrollAnimationFrame) {
+    cancelAnimationFrame(scrollAnimationFrame);
+    scrollAnimationFrame = null;
+  }
 }
 function updateScrolling() {
-    const dir = directionUp - directionDown;
-    if (dir === 0) {
-        stopScrolling();
-        return;
-    }
-    if (scrollAnimationFrame) return;
-    scrollElement = document.querySelector(window.SCROLL_SELECTOR);
-    if (dir > 0) {
-        scrollUp();
-    } else {
-        scrollDown();
-    }
+  const dir = directionUp - directionDown;
+  if (dir === 0) {
+    stopScrolling();
+    return;
+  }
+  if (scrollAnimationFrame) return;
+  scrollElement = document.querySelector(window.SCROLL_SELECTOR);
+  if (dir > 0) {
+    scrollUp();
+  } else {
+    scrollDown();
+  }
 }
 
-window.addEventListener('keydown', function (e) {
+window.addEventListener(
+  "keydown",
+  function (e) {
     const special = (e.ctrlKey || e.altKey) && !e.shiftKey;
-    if (special && (e.code === 'KeyW' || e.code === 'KeyE')) {
-        if (!e.repeat) {
-            directionUp = 1;
-            updateScrolling();
-        }
-        e.preventDefault();
+    if (special && e.code === "KeyE") {
+      if (!e.repeat) {
+        directionUp = 1;
+        updateScrolling();
+      }
+      e.preventDefault();
     }
-    if (special && e.code === 'KeyD') {
-        if (!e.repeat) {
-            directionDown = 1;
-            updateScrolling();
-        }
-        e.preventDefault();
+    if (special && e.code === "KeyR") {
+      if (!e.repeat) {
+        directionDown = 1;
+        updateScrolling();
+      }
+      e.preventDefault();
     }
     const justKey = !e.altKey && !e.shiftKey && !e.ctrlKey && !e.metaKey;
-    const isPaste = (e.ctrlKey || e.metaKey) && e.code === 'KeyV';
-    
-    if ((window.AUTO_FOCUS_ON_ANY_KEY && justKey) || isPaste) {
-        if (this.document.activeElement.tagName !== 'INPUT' && this.document.activeElement.tagName !== 'TEXTAREA') {
-            const inputField = this.document.querySelector(window.INPUT_SELECTOR);
-            if (inputField) {
-                inputField.focus();
-            }
-        }
-    }
-}, true);
+    const isPaste = (e.ctrlKey || e.metaKey) && e.code === "KeyV";
 
-window.addEventListener('keyup', function (e) {
-    if (e.code === 'KeyW' || e.code === 'KeyE') {
-        directionUp = 0;
-        updateScrolling();
+    if ((window.AUTO_FOCUS_ON_ANY_KEY && justKey) || isPaste) {
+      if (
+        this.document.activeElement.tagName !== "INPUT" &&
+        this.document.activeElement.tagName !== "TEXTAREA"
+      ) {
+        const inputField = this.document.querySelector(window.INPUT_SELECTOR);
+        if (inputField) {
+          inputField.focus();
+        }
+      }
     }
-    if (e.code === 'KeyD') {
-        directionDown = 0;
-        updateScrolling();
+  },
+  true,
+);
+
+window.addEventListener(
+  "keyup",
+  function (e) {
+    if (e.code === "KeyE") {
+      directionUp = 0;
+      updateScrolling();
+    }
+    if (e.code === "KeyR") {
+      directionDown = 0;
+      updateScrolling();
     }
     if (!e.ctrlKey && !e.altKey) {
-        directionUp = 0;
-        directionDown = 0;
-        updateScrolling();
+      directionUp = 0;
+      directionDown = 0;
+      updateScrolling();
     }
-}, true);
+  },
+  true,
+);
 
-window.addEventListener('blur', () => {
+window.addEventListener(
+  "blur",
+  () => {
     directionUp = 0;
     directionDown = 0;
-    updateScrolling();    
-}, true);
+    updateScrolling();
+  },
+  true,
+);
